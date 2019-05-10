5710366ebd46a8b300a987606d9f94d2692306e5
Knik <hack2hire01@hack2hire.net>
2019-05-10 13:19:10 +0530

    Adding Script

---
+++ DatabaseScripts/dbo.Movies.sql
@@ -0,0 +1,20 @@
+﻿USE [IMDB]
+GO
+
+/****** Object: Table [dbo].[Movies] Script Date: 10-05-2019 13:17:08 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Movies] (
+    [MovieId]     INT PRIMARY KEY IDENTITY (1, 1) NOT NULL,
+    [Name]        VARCHAR (200) NOT NULL,
+    [ReleaseDate] DATETIME      NULL,
+    [ImgPath]     VARCHAR (MAX) NULL,
+    [Plot]        VARCHAR (MAX) NULL,
+    [ProducerId]  INT           NOT NULL
+);
+
+
